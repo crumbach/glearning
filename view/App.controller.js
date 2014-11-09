cb0ce06ef289c6fcf0ef92c0ef2c1e51e798966f
crumbach <crumbach@gmail.com>
2014-11-09 21:20:27 +0000

    code cleanup

--- view/App.controller.js
+++ view/App.controller.js
@@ -4,7 +4,7 @@ sap.ui.controller("sap.ui.demo.logbook.view.App", {
  		var view = this.getView();
 
  		// to avoid scrollbars on desktop the root view must be set to block display
- 		this.getView().setDisplayBlock(true);
+ 		view.setDisplayBlock(true);
 
  		// apply compact mode if touch is not supported
  		// as the "App" is the root view, all views will inherit this property. 
