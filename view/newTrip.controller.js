ec4db88649d2ae482036ede6fe36776a9f16b125
crumbach <crumbach@gmail.com>
2014-11-12 21:23:50 +0000

    newTrip shows initially logged trips. However, subsequent updates are not shown because neither onInit nor onBeforeRendering are triggered more than once.

--- view/newTrip.controller.js
+++ view/newTrip.controller.js
@@ -2,30 +2,42 @@ sap.ui.controller("sap.ui.demo.logbook.view.newTrip", {
     // dates selected on the calendar are stored on the model as "selectedDates"
     // only upon save, these are moved to the actual logbook 
 
-    // onInit : function() {
-    //     this.initCalendar();
-    // },
-    
-	onBeforeRendering : function() {
-	    this.initCalendar();
-	},
-	
-	initCalendar : function() {
-	    var oModel = this.getView().getModel("baseModel");
-	    if (!oModel) {
-    	    oModel = sap.ui.getCore().getModel("baseModel");
-	    }
-	    console.log("newTrip--onInit/model: " + oModel);
+    oFormatYyyymmdd: null,
 
+    onInit : function() {
+        this.oFormatYyyymmdd = sap.ui.core.format.DateFormat.getInstance({pattern: "yyyy-MM-dd"});
+        
+        var oLegend = this.getView().byId("idLegend");
+        oLegend.addItem(new sap.ui.unified.CalendarLegendItem({
+            text : "Logged trips"
+        }));
+    },
+    
+    onBeforeRendering : function() {
+
+		this.getView().byId("selectedDatesList").attachEventOnce("updateFinished", this.updateFinished, this);
+    },
+    
+    updateFinished : function() {
+		this.initCalendar();
+	},
+		
+	initCalendar : function() {
         var oCalendar = this.getView().byId("calendar");
         oCalendar.removeAllSelectedDates();
-
-        // // oCalendar.addSpecialDate({ "datum": Date.now() });
-        
-        oCalendar.addSpecialDate(new sap.ui.unified.DateTypeRange({
-            type:      sap.ui.unified.CalendarDayType.Type01,
-            startDate: new Date()
-        }));
+            
+	    var oModel = this.getView().byId("selectedDatesList").getModel();
+	    if (oModel) {
+            var logbooks = oModel.getData().logbooks;
+            for (var i = 0; i < logbooks.length; i++) {
+                for (var j = 0; j < logbooks[i].trips.length; j++) {
+                    oCalendar.addSpecialDate(new sap.ui.unified.DateTypeRange({
+                        type:      sap.ui.unified.CalendarDayType.Type01,
+                        startDate: new Date(logbooks[i].trips[j].datum)
+                    }));
+                }
+            }
+	    }
 	},
 
 	onNavBack : function() {
@@ -33,22 +45,22 @@ sap.ui.controller("sap.ui.demo.logbook.view.newTrip", {
 	},
 	
 	onSave : function() {
-        sap.m.MessageToast.show("Saving data");
-
 	    // get base model
 	    var oModel = this.getView().getModel();
 	    var oData = oModel.getData("selectedDates");
 
         // get selected dates from model
         var aCalendarDates = oData.selectedDates;
+        var oDate = null;
         if (aCalendarDates.length > 0) {
             // extend base dates with calendar dates
             for (var i = 0; i < aCalendarDates.length; i++) {
-                var logbook = this.getLogbook(oData.logbooks, aCalendarDates[i].datum.getFullYear());
+                oDate = new Date(aCalendarDates[i].datum);
+                var logbook = this.getLogbook(oData.logbooks, oDate.getFullYear());
+                // logbook.trips.push({"datum": aCalendarDates[i].datum });
                 logbook.trips.push({"datum": aCalendarDates[i].datum });
             }
             // replace selected dates in base model
-            // oModel.setData({"selectedDates": aSelectedDates}, true );
             oModel.setData({"logbooks": oData.logbooks}, true);
         }
         
@@ -109,9 +121,9 @@ sap.ui.controller("sap.ui.demo.logbook.view.newTrip", {
 
         // extend base dates with calendar dates
 	    var aCalendarDates = oEvent.oSource.getSelectedDates();
-        aCalendarDates.forEach(function(aDate) {
-            aSelectedDates.push({"datum": aDate.getStartDate() });
-        });
+        for (var i = 0; i < aCalendarDates.length; i++) {
+            aSelectedDates.push({"datum": this.oFormatYyyymmdd.format(aCalendarDates[i].getStartDate()) });
+        }
 
         // replace selected dates in base model
         oModel.setData({"selectedDates": aSelectedDates}, true );
